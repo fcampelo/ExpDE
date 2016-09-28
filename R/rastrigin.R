46925d42b3658573cfe7d1fb0c2681d05ec29645
Felipe Campelo <fcampelo@ufmg.br>
2016-09-28 18:29:43 -0300

    Update rastrigin.R

--- R/rastrigin.R
+++ R/rastrigin.R
@@ -6,21 +6,12 @@ rastrigin <- function(Pop,...){
 
 rastfun <- function(x){
   n <- length(x);
-  f <- 0;
-  r <- 50;
-  s <- 50;
-
+  A <- 10
+  f <- A * n;
+  
   for (k in 1:n) {
-    f <- f + (x[k] ^ 2 - 10 * cos(2 * pi * x[k]));
+    f <- f + (x[k] ^ 2 - A * cos(2 * pi * x[k]));
   }
 
-  f1 <- 0;
-  f2 <- 0;
-
-  for (p in 1:n) {
-    f1 <- f1 + max(0, (sin(2 * pi * x[p]) + 0.5)); #menor igual
-    f2 <- f2 + abs(cos(2 * pi * x[p]) + 0.5); #igualdade
-  }
-  out <- f + r * f1 + s * f2;
-  return (out);
+  return (f);
 }
