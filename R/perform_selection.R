cd7a0dfba7b657c427d4fe946c653d295e4d2f4e
Bruna Queiroz <bsqueiroz98@gmail.com>
2018-12-04 16:03:51 -0200

    Creation of function "perform_selection"

---
+++ R/perform_selection.R
@@ -0,0 +1,23 @@
+perform_selection <- function(L) {
+  selpars <- L$selpars
+  
+  # ========== Error catching and default value definitions
+  assertthat::assert_that(is.matrix(L$X), is.numeric(L$X),
+                          is.matrix(L$U), is.numeric(L$U),
+                          is.numeric(L$J), is.numeric(L$G),
+                          assertthat::are_equal(dim(L$X), dim(L$U)),
+                          length(L$J) == nrow(L$X), 
+                          length(L$G) == nrow(L$U))
+  # ========== 
+  
+  L$nextpop <- do.call(selpars$name,
+                       args = list(X = L$X,
+                                   U = L$U, 
+                                   J = L$J,
+                                   G = L$G))
+  
+  
+  
+  selpars <- selpars
+  return(L)
+}
